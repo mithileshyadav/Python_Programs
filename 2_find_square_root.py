b06e1384a5ffd1031cb7c47d049ab177fb2eb8ce
mithileshyadav <1245mithileshyadav@gmail.com>
2024-04-06 04:55:43 +0530

    Squre Root

---
+++ 2_find_square_root.py
@@ -0,0 +1,13 @@
+"""
+What is squre root? 
+=======================
+Square root means vergmul
+64 ka vergmull is 8
+किसी संख्या का वर्गमूल वह संख्या है जिसे मूल संख्या के बराबर करने के लिए स्वयं से गुणा किया जा सकता है।
+उदाहरण के लिए, 2, 4 का वर्गमूल है क्योंकि 2x2=4
+√16 का वर्गमूल 4 होगा
+using exponentiation operator means **
+"""
+# 1. USIING EXPONENTION **
+number = int(input("Enter Number for find Squre Root: "))
+print(f"Square root of {number} is: ",number**(1/2))
